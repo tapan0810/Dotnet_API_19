982cb11db4d086c217497776dbbbf12f28a748c1
tapan0810 <arceus0810@gmail.com>
2026-02-28 21:57:05 +0530

    feat :Done with Migration

---
+++ Dotnet_API_19/Data/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Dotnet_API_19.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_19.Data
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options):base (options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Dotnet_API_19/Migrations/20260228162603_1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dotnet_API_19.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Aage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
