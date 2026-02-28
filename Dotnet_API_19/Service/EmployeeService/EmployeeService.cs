3d14534f089b0433ad0971d75799ace8d3297d52
tapan0810 <arceus0810@gmail.com>
2026-02-28 22:05:10 +0530

    feat: Implemented EmployeeServices, Include Pagination for accessing all the employees

---
+++ Dotnet_API_19/Service/EmployeeService/EmployeeService.cs
@@ -0,0 +1,30 @@
+﻿using Dotnet_API_19.Data;
+using Dotnet_API_19.Entities.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_19.Service.EmployeeService
+{
+    public class EmployeeService(EmployeeDbContext _context) : IEmployeeService
+    {
+        public async Task<List<GetAllEmployeesDto>> GetAllEmployees(int pageNumber, int pageSize)
+        {
+            var employees = await _context.Employees
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .Select(s => new GetAllEmployeesDto
+                    {
+                        EmployeeName = s.EmployeeName,
+                        Department = s.Department,
+                        Aage = s.Aage,
+                        isActive = s.isActive
+                    }).ToListAsync();
+
+            if(employees is null || employees.Count == 0)
+            {
+                return null;
+            }
+            return employees;
+
+        }
+    }
+}
---
+++ Dotnet_API_19/Service/EmployeeService/IEmployeeService.cs
@@ -0,0 +1,9 @@
+﻿using Dotnet_API_19.Entities.Dtos;
+
+namespace Dotnet_API_19.Service.EmployeeService
+{
+    public interface IEmployeeService
+    {
+        public Task<List<GetAllEmployeesDto>> GetAllEmployees(int pageNumber,int pageSize);
+    }
+}
