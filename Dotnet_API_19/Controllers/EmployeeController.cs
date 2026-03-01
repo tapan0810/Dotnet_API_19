53f45c9b505b8407c85a1e0db44e9cdb071d5755
tapan0810 <arceus0810@gmail.com>
2026-03-01 08:44:42 +0530

    feat: EmployeeController implemented

---
+++ Dotnet_API_19/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Dotnet_API_19.Entities.Dtos;
+using Dotnet_API_19.Service.EmployeeService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_API_19.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController(IEmployeeService _employee) : ControllerBase
+    {
+        [HttpGet("GetAllEmployees")]
+        public async Task<ActionResult<List<GetAllEmployeesDto>>> GetAllEmployee(int pageNumber,int pageSize)
+        {
+            var employees = await _employee.GetAllEmployees(pageNumber, pageSize);
+            if (employees == null || employees.Count == 0)
+            {
+                return NotFound("No employees found.");
+            }
+            return Ok(employees);
+        }
+    }
+}
